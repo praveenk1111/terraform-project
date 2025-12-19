497b74296ff87ce93c02edb749f11aa8e14ed91b
praveenk1111 <praveenl1111>
2025-12-19 12:28:40 +0530

    modified code, added terraform

--- main.tf
+++ main.tf
@@ -6,29 +6,42 @@ terraform {
 
   required_providers {
     google = {
-        source = "hashicorp/google"
-        version = "-> 5.0"
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "google" {
-    project = var.project_id
-    region = var.region
-    zone = var.zone 
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_instance" "gcp-vm" {
-  name = var.vm_name
+  name         = var.vm_name
   machine_type = var.machine_type
-  zone = var.zone
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-        image = "ubuntu-os-cloud/ubuntu-2204-lts"
-        size = 20
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 20
+      type  = "pd-balanced"
     }
   }
+  network_interface {
+    network = "default"
+
+    access_config {
+      #publc IP
+    }
+  }
+
+  tags = ["terraform", "gcp-vm"]
+
+  labels = {
+    environment = "dev"
+    owner       = "terrafrom"
+  }
 }
-
-
--- variables.tf
+++ variables.tf
@@ -2,30 +2,30 @@
 #### variables ###
 ##################
 variable "project_id" {
-    description = "VM Project ID"
-    type = string
+  description = "VM Project ID"
+  type        = string
 }
 
 variable "region" {
-    description = "Region of GCP"
-    type = string
-    default = "us-central1"
+  description = "Region of GCP"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "zone" {
-    description = "GCP Zone"
-    type = string
-    default = "us-central1-a"
+  description = "GCP Zone"
+  type        = string
+  default     = "us-central1-a"
 }
 
 variable "vm_name" {
-    description = "VM name"
-    type = string
-    default = "GCP-VM" 
+  description = "VM name"
+  type        = string
+  default     = "GCP-VM"
 }
 
 variable "machine_type" {
-    description = "Machine type"
-    type = string
-    default = "e2-medium"
+  description = "Machine type"
+  type        = string
+  default     = "e2-medium"
 }
