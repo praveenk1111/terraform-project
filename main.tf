cecec53da50a3c7f9491cd71d1222dbb78f668c2
praveenk1111 <praveenl1111>
2025-12-19 11:06:54 +0530

    files moved to new repo

---
+++ main.tf
@@ -0,0 +1,34 @@
+################
+## mains file ##
+################
+terraform {
+  required_version = ">=1.5.0"
+
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "-> 5.0"
+    }
+  }
+}
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+    zone = var.zone 
+}
+
+resource "google_compute_instance" "gcp-vm" {
+  name = var.vm_name
+  machine_type = var.machine_type
+  zone = var.zone
+
+  boot_disk {
+    initialize_params {
+        image = "ubuntu-os-cloud/ubuntu-2204-lts"
+        size = 20
+    }
+  }
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,31 @@
+##################
+#### variables ###
+##################
+variable "project_id" {
+    description = "VM Project ID"
+    type = string
+}
+
+variable "region" {
+    description = "Region of GCP"
+    type = string
+    default = "us-central1"
+}
+
+variable "zone" {
+    description = "GCP Zone"
+    type = string
+    default = "us-central1-a"
+}
+
+variable "vm_name" {
+    description = "VM name"
+    type = string
+    default = "GCP-VM" 
+}
+
+variable "machine_type" {
+    description = "Machine type"
+    type = string
+    default = "e2-medium"
+}
